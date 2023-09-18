b5b64313a3b63d9630c943ccdfc8f050a4c9468a
JAYDUTT PATEL <jayduttpatel@JAYDUTTs-MacBook-Pro.local>
2023-09-18 00:42:15 -0400

    first commit

---
+++ src/assets/constants/const.jsx
@@ -0,0 +1,40 @@
+export const StackData = [
+  {
+    id: 1,
+    name: "Creative",
+    content: "We offer creative solution to our customer",
+  },
+  {
+    id: 2,
+    name: "Simple & Secure",
+    content: "All our procedures are carefully tested",
+  },
+  {
+    id: 3,
+    name: "Online & Future-proof",
+    content: "Our unique approach makes all of our partnered dealership",
+  },
+  {
+    id: 4,
+    name: "Shared",
+    content: "Our shared economy values a win win ",
+  },
+];
+
+export const ReviewData = [
+  {
+    id: 1,
+    name: "Emily Thompson",
+    review: "Uobo will be transforming the autmotive industry",
+  },
+  {
+    id: 2,
+    name: "Daniel Richards",
+    review: "As a delaership owner, Uobo revolutionized our business!",
+  },
+  {
+    id: 3,
+    name: "Alex Cooper",
+    review: "Amazing platform, can't wait for the launch!",
+  },
+];
